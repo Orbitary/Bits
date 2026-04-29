9a6cfd29f580a91f24ada07657290eec97a50c5e
ImBit <151650938+ImBit@users.noreply.github.com>
2026-04-29 16:39:51 +0100

    ✨ Added MCConstants.

---
+++ minecraft/src/main/java/xyz/bitsquidd/bits/mc/MCConstants.java
@@ -0,0 +1,15 @@
+/*
+ * This file is part of a Bit libraries package.
+ * Licensed under the GNU Lesser General Public License v3.0.
+ *
+ * Copyright (c) 2023-2026 ImBit
+ */
+
+package xyz.bitsquidd.bits.mc;
+
+public final class MCConstants {
+    private MCConstants() {}
+
+    public static final float PIXEL_LENGTH = 1.0f / 16.0f;
+
+}
