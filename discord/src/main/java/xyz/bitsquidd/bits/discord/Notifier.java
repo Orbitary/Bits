fde7fc9204ef1dc9a0a0fa44a0db7a106ed7d79f
ImBit <151650938+ImBit@users.noreply.github.com>
2026-05-24 14:50:21 +0100

    :bug: Fix notifier `? extends MessageEmbed`.

--- discord/src/main/java/xyz/bitsquidd/bits/discord/Notifier.java
+++ discord/src/main/java/xyz/bitsquidd/bits/discord/Notifier.java
@@ -7,7 +7,6 @@
 
 package xyz.bitsquidd.bits.discord;
 
-import net.dv8tion.jda.api.components.Component;
 import net.dv8tion.jda.api.components.MessageTopLevelComponent;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
@@ -27,23 +26,23 @@ public final class Notifier {
             send(userId, List.of(embed), List.of());
         }
 
-        public static void send(long userId, List<MessageEmbed> embeds) {
+        public static void send(long userId, List<? extends MessageEmbed> embeds) {
             send(userId, embeds, List.of());
         }
 
-        public static void send(long userId, MessageEmbed embed, List<MessageTopLevelComponent> components) {
+        public static void send(long userId, MessageEmbed embed, List<? extends MessageTopLevelComponent> components) {
             send(userId, List.of(embed), components);
         }
 
-        public static void send(long userId, List<MessageEmbed> embeds, List<MessageTopLevelComponent> components) {
+        public static void send(long userId, List<? extends MessageEmbed> embeds, List<? extends MessageTopLevelComponent> components) {
             BitsDiscord.jda().openPrivateChannelById(userId).queue(
-                channel -> channel.sendMessageEmbeds(embeds)
-                    .setComponents(components)
-                    .queue(
-                        null,
-                        err -> Logger.warn("Failed to send DM to " + userId + ": " + err.getMessage())
-                    ),
-                err -> Logger.warn("Could not open DM channel for " + userId + ": " + err.getMessage())
+              channel -> channel.sendMessageEmbeds(embeds)
+                .setComponents(components)
+                .queue(
+                  null,
+                  err -> Logger.warn("Failed to send DM to " + userId + ": " + err.getMessage())
+                ),
+              err -> Logger.warn("Could not open DM channel for " + userId + ": " + err.getMessage())
             );
         }
 
